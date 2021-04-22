8d4cac58f65aefebb3f795de1d0639f4b4b8cdf2
chyw2 <chyw2@DESKTOP-I70680A>
2021-04-22 13:54:47 +0900

    마스터

---
+++ project5jo/project-re/src/controller/MainController.java
@@ -0,0 +1,72 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.Action;
+import service.ActionForward;
+
+/**
+ * Servlet implementation class MainController
+ */
+@WebServlet("*.do")
+public class MainController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = requestURI.substring(contextPath.length());
+		
+		System.out.println("requestURI:"+requestURI);
+		System.out.println("contextPate:"+contextPath);
+		System.out.println("command:"+command);
+		
+		Action action = null;
+		ActionForward forward = null;
+		
+		
+		
+		
+		
+		// forwarding 처리
+		if(forward != null) {
+			if(forward.isRedirect()) {	// redirect 방식으로 포워딩
+				response.sendRedirect(forward.getPath());
+			} else {					// dispatcher 방식으로 포워딩
+				RequestDispatcher dispatcher =
+						request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+		}
+		
+	} // doProcess() end
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Get");
+		
+		doPost(request, response);
+	} 
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Post");
+		
+		doPost(request, response);
+	}
+
+}
